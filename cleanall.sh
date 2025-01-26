ba5191b148fe5c550b3756938a6883089ebfd20d
Phill Harvey-Smith <afra@ramoth.org.uk>
2025-01-26 19:07:57 +0000

    Added apple_iic and amstrad_cpc to cleanall script

--- cleanall.sh
+++ cleanall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TARGETS="atom bbc bbcmaster commodore dragon_coco dragon_beta electron spectrum trs80 zx81"
+TARGETS="amstrad_cpc apple_iic atom bbc bbcmaster commodore dragon_coco dragon_beta electron spectrum trs80 zx81"
 
 for TARGET in $TARGETS
 do
